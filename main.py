454d5f8aacb46f3cb15d39b7c1a13050b82dc9bb
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-07-13 20:26:23 +0700

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+from model import detector
+
+model = detector()
+
+print("Please enter your sentence: (Press Enter twice to start processing)")
+contents = []
+while True:
+    line = input()
+    if len(line) == 0:
+        break
+    contents.append(line)
+sentence = "\n".join(contents)
+
+print(f"Result: {model(sentence)[-1]}")
